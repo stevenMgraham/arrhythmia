5229ce4678793ad2c13507c0e3f019e4680e7711
stevenMgraham <41012057+stevenMgraham@users.noreply.github.com>
2021-10-31 00:07:24 -0400

    Creation of repo

---
+++ data_exploration.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import the dataset .csv as DataFrame
+arrhythmia_df = pd.read_csv('arrhythmia_with_column_names.csv')
+print(arrhythmia_df.head())
+
